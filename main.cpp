55b2aed808b112559d897d1def0a6a8ec398c1d4
klewy <cigankov807@gmail.com>
2025-12-24 19:21:09 +0300

    A bit of business logic

--- main.cpp
+++ main.cpp
@@ -9,49 +9,90 @@
 #include <boost/beast/http/dynamic_body_fwd.hpp>
 #include <boost/beast/http/message.hpp>
 #include <boost/beast/http/dynamic_body.hpp>
+#include <boost/beast/http/message_fwd.hpp>
 #include <boost/beast/http/parser.hpp>
 #include <boost/beast/http/read.hpp>
 #include <boost/beast/http/serializer.hpp>
+#include <boost/beast/http/status.hpp>
 #include <boost/beast/http/string_body_fwd.hpp>
+#include <boost/beast/http/verb.hpp>
 #include <boost/beast/http/write.hpp>
 #include <boost/beast/http/string_body.hpp>
 #include <boost/beast/core/flat_buffer.hpp>
 #include <boost/system/detail/error_code.hpp>
 #include <chrono>
 #include <cstddef>
+#include <nlohmann/detail/conversions/from_json.hpp>
+#include <nlohmann/json_fwd.hpp>
 #include <print>
 #include <memory>
+#include <unordered_map>
+#include <nlohmann/json.hpp>
 
-boost::beast::http::response<boost::beast::http::string_body> process_request(const boost::beast::http::request<boost::beast::http::string_body>& msg) {
-    boost::beast::http::message<false, boost::beast::http::string_body> send_msg;
-    if (msg.target().starts_with("/something")) {
-        send_msg.body() = "Something";
-    } else {
-        send_msg.body() = "Other";
+using response = boost::beast::http::response<boost::beast::http::string_body>;
+using request = boost::beast::http::request<boost::beast::http::string_body>;
+
+class Bank {
+    std::unordered_map<std::string, int> bank_accounts_;
+public:
+    response route(const request& req) {
+        response result;
+        if (req.target() == "/echo" && req.method() == boost::beast::http::verb::post) {
+            result = route_echo(req);
+        } else if (req.target() == "/add-money" && req.method() == boost::beast::http::verb::post) {
+            result = route_add_money(req);
+        } else {
+            result.clear();
+            result.body() = "";
+            result.result(boost::beast::http::status::ok);
+        }
+        return result;
     }
-    // send_msg.keep_alive(msg.keep_alive());
-    send_msg.result(boost::beast::http::status::ok);
-    send_msg.prepare_payload(); // This is called if a message contains body
-    return send_msg;
-}
+private:
+    response route_echo(const request& req) {
+        auto body = req.body();
+        nlohmann::json json = nlohmann::json::parse(body);
+        auto message = json["message"];
 
-struct Session : public std::enable_shared_from_this<Session> {
+        response resp;
+        resp.body() = message;
+        resp.result(boost::beast::http::status::ok);
+        resp.prepare_payload();
+        return resp;
+    }
+
+    response route_add_money(const request& req) {
+        nlohmann::json json = nlohmann::json::parse(req.body());
+        auto name = nlohmann::to_string(json["username"]);
+        int value = std::stoi(nlohmann::to_string(json["value"]));
+        bank_accounts_[name] += value;
+
+        response resp;
+        resp.body() = std::format("Balance of {} is {}$", name, bank_accounts_[name]);
+        resp.prepare_payload();
+        return resp;
+    }
+};
+
+class Session : public std::enable_shared_from_this<Session> {
     boost::beast::tcp_stream stream_;
     boost::beast::flat_buffer buf_;
-    boost::beast::http::request<boost::beast::http::string_body> recv_msg_;
-    boost::beast::http::response<boost::beast::http::string_body> send_msg_;
+    request recv_msg_;
+    response send_msg_;
 
+    Bank bank_;
+public:
     Session(boost::asio::ip::tcp::socket&& sock) : stream_(std::move(sock)) {}
 
     void process() {
-        stream_.expires_after(std::chrono::seconds(5)); // Drops connection after specified time
+        stream_.expires_after(std::chrono::seconds(30)); // Drops connection after specified time
         // shared_from_this to keep object alive, since it is supposed to be dead at this point in Server::start_accept
         boost::beast::http::async_read(stream_, buf_, recv_msg_, std::bind(&Session::do_read, shared_from_this(), boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
     }
 
     void do_read(const boost::system::error_code& err, std::size_t bytes_tf) {
         if (!err) {
-            send_msg_ = process_request(recv_msg_);
+            send_msg_ = bank_.route(recv_msg_);
             boost::beast::http::async_write(stream_, send_msg_, std::bind(&Session::do_write, shared_from_this(), boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
         } else {
             std::println("Read failed: {}", err.message());
@@ -59,6 +100,7 @@ struct Session : public std::enable_shared_from_this<Session> {
     }
     void do_write(const boost::system::error_code& err, std::size_t bytes_tf) {
         if (!err) {
+            std::println("Written");
             if (!recv_msg_.keep_alive()) {
                 do_close();
                 return;
@@ -81,7 +123,7 @@ class Server {
     boost::asio::ip::tcp::acceptor acceptor_;
 public:
     Server(boost::asio::io_context& io, boost::asio::ip::address addr, unsigned short port) : io_(io), acceptor_(io, {addr, port}) {
-        acceptor_.set_option(boost::asio::socket_base::reuse_address{});
+        acceptor_.set_option(boost::asio::socket_base::reuse_address{true});
         start_accept();
     }
 
@@ -99,7 +141,7 @@ public:
 
 int main(int, char**){
     boost::asio::io_context io;
-    Server serv{io, boost::asio::ip::make_address("127.0.0.1"), 8080};
+    Server serv{io, boost::asio::ip::make_address("127.0.0.1"), 8000};
     io.run();
     return 0;
 }
